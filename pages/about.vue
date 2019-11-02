4813d6f23d9df70ca050355dc6f4dde696fe5615
kou0402 <kou.4869.biz@gmail.com>
2019-11-03 00:05:51 +0900

    style: update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,34 @@
 <template>
-  <section>
-    自己紹介
+  <section class="about">
+    <h1 class="name">鈴木晃平 / KOHEI SUZUKI</h1>
+    <p class="introduction">
+      1992年4月2日、北海道生まれ埼玉育ち。<br />
+      2016年、成城大学経済学部卒業後、<br />
+      新卒でオフィス家具商社の営業職として入社。<br />
+      プログラミングに興味を持ち、退職後、職業訓練校にてJavaを学習。<br />
+      その後2018年1月にエスワイシステムに入社。<br />
+      <br />
+      現在、荻窪に在住。<br />
+      趣味は音楽と個人開発で、最近はFirebaseとNuxt.jsを主に使用して開発。<br />
+      このサイトもFirebaseとNuxt.jsを使用しており、また、GitHubとCircleCIによる<br />
+      自動デプロイ機構が組まれている。
+    </p>
   </section>
 </template>
 
 <style lang="scss" scoped>
-section {
-  font-size: 3rem;
+.about {
+  letter-spacing: 3px;
+  > .name {
+    color: $main-color1;
+    font-size: 3rem;
+    font-weight: 500;
+    margin-bottom: 30px;
+  }
+  > .introduction {
+    color: $main-color1;
+    font-size: 2rem;
+    line-height: 30px;
+  }
 }
 </style>
