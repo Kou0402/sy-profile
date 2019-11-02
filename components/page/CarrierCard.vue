a9d8b3f57c2c3c6af9ddd429b4ed959d6e3f1a05
kou0402 <kou.4869.biz@gmail.com>
2019-11-03 02:09:10 +0900

    style: update carrier page

---
+++ components/page/CarrierCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <article class="carrier-card">
+    <h2 class="title">
+      {{ title }} <span>（期間：{{ period }}ヶ月）</span>
+    </h2>
+    <h3 class="subtitle">
+      概要
+    </h3>
+    <p class="paragraph">
+      {{ overview }}
+    </p>
+    <h3 class="subtitle">
+      技術
+    </h3>
+    <p class="paragraph">
+      {{ technology }}
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    period: {
+      type: Number,
+      required: true
+    },
+    overview: {
+      type: String,
+      required: true
+    },
+    technology: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.carrier-card {
+  color: $main-color1;
+  > .title {
+    font-size: 3.2rem;
+    font-weight: 500;
+    margin-bottom: 20px;
+  }
+  > .subtitle {
+    font-size: 2.4rem;
+    font-weight: 500;
+    margin-bottom: 10px;
+  }
+  > .paragraph {
+    font-size: 1.9rem;
+    margin-bottom: 17px;
+  }
+}
+</style>
--- pages/carrier.vue
+++ pages/carrier.vue
@@ -1,11 +1,76 @@
 <template>
-  <section>
-    職務経歴
+  <section class="carrier">
+    <CarrierCard
+      v-for="item in carrierData"
+      :key="item.id"
+      :title="item.title"
+      :period="item.period"
+      :overview="item.overview"
+      :technology="item.technology"
+    ></CarrierCard>
   </section>
 </template>
 
+<script>
+import CarrierCard from '~/components/page/CarrierCard.vue'
+
+export default {
+  components: {
+    CarrierCard
+  },
+  data() {
+    return {
+      carrierData: [
+        {
+          title: '不動産会社営業向けシステム改修',
+          period: 2,
+          overview:
+            '不動産会社の営業向け顧客管理パッケージソフトの改修案件。' +
+            '顧客検索部分の新機能ページの追加を担当。',
+          technology: 'PHP/JavaScript/Lumen/jQuery/PostgreSQL/Linux/Vagrant'
+        },
+        {
+          title: 'ポータルサイトのデータ変換ツール',
+          period: 1,
+          overview:
+            'VBA案件。とある機関のポータルサイト改修プロジェクトの一環で、サイトに' +
+            '掲示するための資料データを、規格通りに整形するエクセルツールの作成を担当。',
+          technology: 'VBA'
+        },
+        {
+          title: '大手複合機メーカー基幹システム開発テストサポート',
+          period: 2,
+          overview:
+            '大手企業の基幹システムの改修プロジェクトのテストサポーターとして参画。' +
+            'テスト用データ作成や不具合発生時のパッチSQL作成などを担当。',
+          technology: 'SQL/OracleDB'
+        },
+        {
+          title: '保証会社契約管理システムリプレイス',
+          period: 9,
+          overview:
+            '金融関連会社の社内システムのリプレイス案件。Flashで構成されたシステムを' +
+            'HTML5/CSS3にて再構成。 主にjQueryによる画面動作部を担当。',
+          technology: 'Java/JavaScript/Seasar2/jQuery/SQLServer'
+        },
+        {
+          title: 'サーバーリプレイス',
+          period: 7,
+          overview:
+            '老朽化したサーバーとソフトウェアのリプレイス案件。' +
+            '主にバッチ系処理の製造を担当。',
+          technology: 'WindowsServer/Java/VBS'
+        }
+      ]
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
-section {
-  font-size: 3rem;
+.carrier {
+  > .carrier-card {
+    margin-bottom: 50px;
+  }
 }
 </style>
